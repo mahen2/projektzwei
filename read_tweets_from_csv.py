01d32e98346b9de96894bf0e84be3ed74bdb18d9
fapeg <fabian@fapeg.com>
2013-11-09 01:45:29 +0100

    hashtag-analyse

--- read_tweets_from_csv.py
+++ read_tweets_from_csv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
+# sort dictionary quelle: http://stackoverflow.com/questions/613183/python-sort-a-dictionary-by-value
 
-import csv
+import csv, operator
 
 from datetime import datetime
 
@@ -17,6 +18,7 @@ with open(csv_file, 'rb') as csvfile:
 
 date_format = "%Y-%m-%d %H:%M:%S"
 date_list1 = []
+hashtag_list1 = []
 
 for tweetlist in all_tweets:
     feldindex=0
@@ -25,6 +27,10 @@ for tweetlist in all_tweets:
         print feld
         if feldindex == 2: # das zweite feld in der csv enthält das datum
             date_list1.append(datetime.strptime(feld, date_format)) # liste mit datetime-objekten
+        if feldindex == 3:
+            for hashtag in feld.split():
+                if hashtag.startswith("#"):
+                    hashtag_list1.append(hashtag)
     print "\n"
 
 tage1 = {}
@@ -33,6 +39,8 @@ jahre1 = {}
 tage1_list = []
 monate1_list = []
 jahre1_list = []
+
+print "\n\n--- Datumsanalyse der Tweets  ---\n\n"
 
 for d in date_list1:
     tage1[d.weekday()]=''
@@ -81,4 +89,24 @@ for key in tage1:
         print "samstags: "  + str(tage1[key]) + " Tweets"
     if key==6:
         print "sonntags: "  + str(tage1[key]) + " Tweets"
-        
+
+hashtag1 = {}
+for hashtag in hashtag_list1:
+    hashtag1[hashtag] = hashtag_list1.count(hashtag)
+
+
+hashtag1_sorted = sorted(hashtag1.iteritems(), key=operator.itemgetter(1))
+
+print "\n\n--- Hashtaganalyse der Tweets (Schwelle: mindestens 10 mal verwendet)  ---\n\n"
+
+
+for tup in hashtag1_sorted:
+    if tup[1]>=10:
+        print tup[0] + ": " + str(tup[1]) + " mal"
+
+
+
+
+
+
+ 
