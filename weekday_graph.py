68a1f8d4d8765e40240afeb345e8f11d5125ca54
fapeg <fabian@fapeg.com>
2013-11-16 02:59:31 +0100

    grafik hochladen und twittern

--- weekday_graph.py
+++ weekday_graph.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import read_tweets_from_csv
+import read_tweets_from_csv, operator
 
 datei = raw_input("Dateinamen angeben: ")
 
@@ -16,6 +16,31 @@ username = datei.split("_tweets.csv")[0]
 days_percent = []
 
 days_whole = sum(tage1.values())
+
+#### das hab ich mal eingefügt um den besten tag zu ermitteln, könnte man bestimmt auch irgendwie einfacher machen:
+tage_sorted = sorted(tage1.iteritems(), key=operator.itemgetter(1))
+h_values = []
+h_keys = []
+for key, value in tage_sorted:
+    h_values.append(value)
+    if key==0:
+        key_s="Monday"
+    if key==1:
+        key_s="Tuesday"
+    if key==2:
+        key_s="Wednesday"
+    if key==3:
+        key_s="Thursday"
+    if key==4:
+        key_s="Friday"
+    if key==5:
+        key_s="Saturday"
+    if key==6:
+        key_s="Sunday"
+    
+    h_keys.append(key_s)
+####
+
 
 for key, value in tage1.iteritems():
     value_percent = 100 * float(value)/float(days_whole)
@@ -28,4 +53,12 @@ colors=['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'blue', 'white','re
 explode=(0, 0, 0, 0, 0, 0, 0) # explode nothing for now
 plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
 plt.axis('equal')
+dateiname = 'weekdays_'+username+'.png'
+plt.savefig(dateiname)
+try:
+    import grafikupload
+    grafikupload.hochladen_und_twittern(dateiname, username, "Wochentag", h_keys[-1])
+except ImportError, e:
+    pass
+
 plt.show()
