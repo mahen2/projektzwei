1151237596eed01dc058753d2c616d78465b84d3
Maria <mariahenkel@gmx.de>
2013-11-19 10:02:12 +0100

    Monate, fast fertig

--- monate_graph.py
+++ monate_graph.py
@@ -7,25 +7,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import read_tweets_from_csv, operator
 
-datei = raw_input("Dateinamen angeben: ")
+#datei1 = raw_input("Dateinamen angeben: ")
+#datei2 = raw_input("Dateinamen angeben: ")
 
-(hashtag1_sorted, mentions1_sorted, clients1_sorted, woerter1_sorted, jahre1, monate1, tage1) = read_tweets_from_csv.analyse_tweets(datei)
-username = datei.split("_tweets.csv")[0]
+datei1 = 'epidota2_tweets.csv'
+datei2 = 'dota2rainbow_tweets.csv'
 
-#monate1 = {'2013-03': 33, '2012-12': 60, '2013-02': 61, '2012-05': 14, '2012-06': 48, '201
-#2-10': 12, '2012-09': 4, '2012-08': 22, '2013-08': 81, '2013-09': 47, '2012-11':
-# 37, '2013-04': 42, '2013-05': 90, '2013-06': 80, '2013-07': 31, '2012-07': 53,
-#'2013-01': 5, '2013-11': 7, '2013-10': 34}
+(hashtag1_sorted, mentions1_sorted, clients1_sorted, woerter1_sorted, jahre1, monate1, tage1) = read_tweets_from_csv.analyse_tweets(datei1)
+(hashtag2_sorted, mentions2_sorted, clients2_sorted, woerter2_sorted, jahre2, monate2, tage2) = read_tweets_from_csv.analyse_tweets(datei2)
 
-#monate1 muss noch bearbeitet werden damit man die daten auslesen kann.
+username1 = datei1.split("_tweets.csv")[0]
+username2 = datei2.split("_tweets.csv")[0]
 
-months_percent = []
+months_percent1 = []
+months_percent2 = []
 
-months_whole = sum(monate1.values())
+def calculate_percent(monate, months_percent):
+    global months_percent1
+    global months_percent2
+    months_whole = sum(monate.values())
+    for key, value in monate.iteritems():
+        value_percent = 100 * float(value)/float(months_whole)
+        months_percent.append(value_percent)
 
-for key, value in monate1.iteritems():
-    value_percent = 100 * float(value)/float(months_whole)
-    months_percent.append(value_percent)
+calculate_percent(monate1, months_percent1)
+calculate_percent(monate2, months_percent2)
 
 
 plt.figure(1)
@@ -33,16 +39,16 @@ labels=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'
 colors=['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'blue', 'white','red', 'yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'blue']
 
 plt.subplot(121)
-sizes=months_percent
+sizes=months_percent1
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
-plt.title('Tweets per Month: '+username)
+plt.title('Tweets per Month: '+username1)
 plt.axis('equal')
 
 
 plt.subplot(122)
-sizes=months_percent
+sizes=months_percent2
 plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True)
-plt.title('Tweets per Month')
+plt.title('Tweets per Month: '+username2)
 plt.axis('equal')
 
 plt.show()
